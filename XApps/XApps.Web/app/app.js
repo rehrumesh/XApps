616d121035ee8957b1e217c0da6b12ab4417106e
rehrumesh <rehrumesh@hotmail.com>
2014-04-10 22:51:42 +0530

    Route Config File added.

--- XApps/XApps.Web/app/app.js
+++ XApps/XApps.Web/app/app.js
@@ -1,25 +1,5 @@
 ﻿var app = angular.module('app', ['ngRoute']);
-app.config(function ($routeProvider) {
 
-    $routeProvider.when("/", {
-        controller: "dashboard",
-        templateUrl: "app/dashboard/dashboard.html"
-    })
-    .when("/myapps", {
-        controller: "myapps",
-        templateUrl: "app/myapps/myapps.html"
-    })
-    .when("/hotapps", {
-        controller: "hotapps",
-        templateUrl: "app/hotapps/hotapps.html"
-    })
-    .when("/dashboard", {
-        controller: "dashboard",
-        templateUrl: "app/dashboard/dashboard.html"
-    });
-    $routeProvider.otherwise({ redirectTo: "/dashboard" });
-
-});
 //
 //default route is http://localhost:6580/
 // want to make it as http://localhost:6580/#/
