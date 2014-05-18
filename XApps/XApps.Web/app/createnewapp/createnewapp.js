b82f712f8db2f8bae88f5c63b15a60f8c0799b97
shandigutt <striderpaw@gamil.com>
2014-05-18 13:30:35 +0530

    on the way finishing open app function

--- XApps/XApps.Web/app/createnewapp/createnewapp.js
+++ XApps/XApps.Web/app/createnewapp/createnewapp.js
@@ -81,6 +81,8 @@
 
     };
 
+    
+
     function getText() {
         alert("asdasdasas");
     };
@@ -173,6 +175,117 @@
         editor.setValue("");
         $scope.currentFileType = "css";
         $scope.currentFilePath = "css/";
+    };
+
+    $scope.openRepo = function () {
+        var userName = $scope.githubUserName;
+        var repoName = prompt("Enter the repo name");
+        var requestFiles;
+        var filesResponseObj;
+        var requestContent;
+        var contentResponseObj;
+        
+        //the following line is to allocate 25mb local storage
+        //var fso = new FSO(1024 * 1024 * 25, false);
+
+        function bringFiles() {
+            filesResponseObj = JSON.parse(requestFiles.responseText);
+            var forLoopVar = 0;
+
+            for (forLoopVar = 0; forLoopVar < Object.keys(filesResponseObj.tree).length; forLoopVar++) {
+                
+                if (filesResponseObj.tree[forLoopVar].type == "blob") {
+                    
+                    function writeContent() {
+                        contentResponseObj = JSON.parse(this.responseText);
+                        /*//another baase64 decoding algorithm
+                        function decode_base64(s) {
+                            var e = {}, i, k, v = [], r = '', w = String.fromCharCode;
+                            var n = [[65, 91], [97, 123], [48, 58], [43, 44], [47, 48]];
+
+                            for (z in n) { for (i = n[z][0]; i < n[z][1]; i++) { v.push(w(i)); } }
+                            for (i = 0; i < 64; i++) { e[v[i]] = i; }
+
+                            for (i = 0; i < s.length; i += 72) {
+                                var b = 0, c, x, l = 0, o = s.substring(i, i + 72);
+                                    for (x = 0; x < o.length; x++) {
+                                        c = e[o.charAt(x)]; b = (b << 6) + c; l += 6;
+                                        while (l >= 8) { r += w((b >>> (l -= 8)) % 256); }
+                                    }
+                            }
+                            return r;
+                        }*/
+                        
+
+                        function decodeBase64(s) {
+                            var e = {}, i, b = 0, c, x, l = 0, a, r = '', w = String.fromCharCode, L = s.length;
+                            var A = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+                            for (i = 0; i < 64; i++) { e[A.charAt(i)] = i; }
+                            for (x = 0; x < L; x++) {
+                                c = e[s.charAt(x)]; b = (b << 6) + c; l += 6;
+                                while (l >= 8) { ((a = (b >>> (l -= 8)) & 0xff) || (x < (L - 2))) && (r += w(a)); }
+                            }
+                            return r;
+                        };
+                        
+                        //writeFile decodeBase64(contentResponseObj.content)
+                        //text = filesResponseObj.tree[i].path + " *** " + decodeBase64(contentResponseObj.content);
+                        alert("file content: "+decodeBase64(contentResponseObj.content));
+                        
+                    }
+                    requestContent = new XMLHttpRequest(); 
+                    requestContent.onload = writeContent;
+                    requestContent.open('get', 'https://api.github.com/repos/Xapps00/' + repoName + '/git/blobs/' + filesResponseObj.tree[forLoopVar].sha, true);
+                    requestContent.send();
+                    
+                    // the following method is to check whether the the request is fetched
+                    // this writes the fetched http request to the text editor
+                    requestContent.onreadystatechange = function () {
+                        if (requestContent.readyState == 4 && requestContent.status == 200) {
+
+                            document.getElementById("editor").innerHTML = requestContent.responseText;
+                        }
+                    }//check method finish from here
+
+                }
+
+            }
+            
+        }
+        requestFiles = new XMLHttpRequest();
+        requestFiles.onload = bringFiles;//.then(function () { alert("came here"); });
+        requestFiles.open('get', 'https://api.github.com/repos/Xapps00/' + repoName + '/git/trees/master?recursive=1', true);//https://api.github.com/users/Xapps00/repos
+        requestFiles.send();
+
+        /*// the following method is tocheck whether the the request is fetched
+        // this writes the fetched http request to the text editor
+        requestFiles.onreadystatechange = function () {
+            if (requestFiles.readyState == 4 && requestFiles.status == 200) {
+
+                document.getElementById("editor").innerHTML = requestFiles.responseText;
+
+            }
+        }*/
+
+
+        /*
+            the problem I want to solve here is,
+
+            first a http call is made and when the data is fetched we call 'bringFiles' function. this function is called asynchronously (line 256)
+            all the file paths in the repository will be loaded to 'filesResponseObj' variable as a json object (line 192)
+            then for each file loaded we call another http call to bring the content of each file and there we call 'writeContent' function. this function is also called asynchronously (ine 237)
+            there we assign all the file content to 'contentResponseObj' variable as a json object
+
+            only when all these asynchronous functions are called we can write content of each file to the local storage.
+            how can we know when all these function are over??
+            in line 256 i tried using then(). but it doesn't seem to be working. coz then 'bringFiles' will not be called
+
+            we have another option also
+            in line 257 and 238 when we open the http call we pass the third parameter as true to say that this all can happen asynchronously
+            coz if we make it false the whole app will be stuck until the data is fetched
+            so I ddin't try doing that
+        */
+
     };
 
 });
