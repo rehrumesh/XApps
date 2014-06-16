ab6b78596d111a1593f7611b49f037d1737b7e54
shandigutt <striderpaw@gamil.com>
2014-06-16 10:40:20 +0530

    user login with github partly done

--- XApps/XApps.Web/app/maincontroller.js
+++ XApps/XApps.Web/app/maincontroller.js
@@ -1,4 +1,4 @@
-﻿app.controller('maincontroller', function ($scope,$compile) {
+﻿app.controller('maincontroller', function ($scope,$compile, $http) {
     $scope.onSideBarLoaded = function () {
        
             $("#browser").treeview({
@@ -10,5 +10,36 @@
             $("#browser").treeview();
            
             $compile($("#browser"))($scope);
+            
+    }
+    /*
+    Client ID
+    739ed9983efe3f0260f5
+    Client Secret
+    9c92dd882f4ca86079a04d2e4a7255be77405213
+    token
+    1aac3b3d9eebde2dc2d1d26844cd461071fdf3bb
+    */
+    $scope.user;
+    $scope.userAccess = "undefined";
+
+    $scope.user_authenticate = function () {
+        OAuth.initialize('gVSwp4XmyIU6A-VfLSeA6Njh_2Q');
+        OAuth.popup('github', function (error, result) {
+            
+            $scope.userAccess = result;
+            
+            $http.get("https://api.github.com/user?access_token=" + $scope.userAccess.access_token)
+                        .success(function (data) {
+                            $scope.user = data;
+                            console.info(JSON.stringify(data));
+                        }).error(function () {
+                            console.error("Error loading user details");
+                        });
+        });
+
+        
+
+        
     }
 });
