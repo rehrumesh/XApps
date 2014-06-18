fcaddf46eee7d81078c4c90a4f6bd46fdc839964
Chamara <wrchamara89@gmail.com>
2014-06-18 11:29:47 +0530

    Rating part 2

--- XApps/XApps.Web/app/dashboard/dashboard.js
+++ XApps/XApps.Web/app/dashboard/dashboard.js
@@ -51,15 +51,15 @@
 
     
 
-    $scope.rating = 0;
-    $scope.ratings = [{
-        current: 1,
-        max: 5
-    }];
+    //$scope.rating = 0;
+    //$scope.ratings = [{
+    //    current: 1,
+    //    max: 5
+    //}];
 
-    $scope.getSelectedRating = function (rating) {
-        console.log(rating);
-    }
+    //$scope.getSelectedRating = function (rating) {
+    //    console.log(rating);
+    //}
     
     /*
     $scope.RatingApp = function () {
@@ -76,43 +76,45 @@
 
 });
 
-app.directive('starRating', function () {
-    return {
-        restrict: 'A',
-        template: '<ul class="rating">' +
-            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
-            '\u2605' +
-            '</li>' +
-            '</ul>',
-        scope: {
-            ratingValue: '=',
-            max: '=',
-            onRatingSelected: '&'
-        },
-        link: function (scope, elem, attrs) {
 
-            var updateStars = function () {
-                scope.stars = [];
-                for (var i = 0; i < scope.max; i++) {
-                    scope.stars.push({
-                        filled: i < scope.ratingValue
-                    });
-                }
-            };
 
-            scope.toggle = function (index) {
-                scope.ratingValue = index + 1;
-                scope.onRatingSelected({
-                    rating: index + 1
-                });
-            };
+//app.directive('starRating', function () {
+//    return {
+//        restrict: 'A',
+//        template: '<ul class="rating">' +
+//            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+//            '\u2605' +
+//            '</li>' +
+//            '</ul>',
+//        scope: {
+//            ratingValue: '=',
+//            max: '=',
+//            onRatingSelected: '&'
+//        },
+//        link: function (scope, elem, attrs) {
 
-            scope.$watch('ratingValue', function (oldVal, newVal) {
-                if (newVal) {
-                    updateStars();
-                }
-            });
-        }
-    }
-});
+//            var updateStars = function () {
+//                scope.stars = [];
+//                for (var i = 0; i < scope.max; i++) {
+//                    scope.stars.push({
+//                        filled: i < scope.ratingValue
+//                    });
+//                }
+//            };
+
+//            scope.toggle = function (index) {
+//                scope.ratingValue = index + 1;
+//                scope.onRatingSelected({
+//                    rating: index + 1
+//                });
+//            };
+
+//            scope.$watch('ratingValue', function (oldVal, newVal) {
+//                if (newVal) {
+//                    updateStars();
+//                }
+//            });
+//        }
+//    }
+//});
 
