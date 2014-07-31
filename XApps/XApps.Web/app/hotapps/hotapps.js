191cc10012f5f0939d17edc177dc3b695272ca2b
rehrumesh <rehrumesh@hotmail.com>
2014-07-31 10:40:32 +0530

    Code commenting and dashboard editing

--- XApps/XApps.Web/app/hotapps/hotapps.js
+++ XApps/XApps.Web/app/hotapps/hotapps.js
@@ -1,3 +1,16 @@
-﻿app.controller('hotapps', function ($scope) {
+﻿/**  
+  * @desc this class will hold functions for HotApps module
+  * 
+  * @author Rumesh Eranga
+  * @author Chamara Jayalath
+  * @author Sahan Mendis
+  * @author Tharaka Viswakula
+  * @author Isuru Anuradha
+  *
+  * @required app.js
+*/
+
+
+app.controller('hotapps', function ($scope) {
     $scope.devName = "Rumesh";
 });
